75c6e74eec1a01832103e7cc7787fba02433cf5d
Fobozzz86 <fobozzz@yandex.ru>
2023-06-21 12:35:31 +0300

    Добавление аннотаций перед/после тестов

---
+++ src/test/java/com/demoqa/FirstJUnitTest.java
@@ -0,0 +1,38 @@
+package com.demoqa;
+
+import org.junit.jupiter.api.*;
+
+public class FirstJUnitTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("#### beforeAll()");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("#### afterAll()");
+    }
+    @BeforeEach
+    void beforeach() {
+        System.out.println("   #### beforeach()");
+}
+
+    @AfterEach
+    void aftereach() {
+        System.out.println("          #### aftereach()");
+    }
+
+   @Test
+   void firstTest() {
+       Assertions.assertTrue(3 > 2);
+       System.out.println("       #### @Test firstTest()");
+   }
+
+    @Test
+    void secondTest() {
+        Assertions.assertTrue(3 > 2);
+        System.out.println("      #### @Test secondTest()");
+    }
+
+}
