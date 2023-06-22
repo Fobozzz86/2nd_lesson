7c68a77f9eb8f82f2a3fd5e1cb26ffd245344d37
Fobozzz86 <fobozzz@yandex.ru>
2023-06-22 16:55:44 +0300

    Add code to Test

--- src/test/java/com/demoqa/PracticeFormTest.java
+++ src/test/java/com/demoqa/PracticeFormTest.java
@@ -1,6 +1,7 @@
 package com.demoqa;
 
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -10,15 +11,24 @@ public class PracticeFormTest extends TestBase {
     @Test
     void vvoddataTest() {
         open("https://demoqa.com/automation-practice-form");
-     //  $("[placeholder=First Name]").setValue("Artem");
-     //   $(".form-control").shouldHave(text("First Name")).setValue("Artem");
-     //   $("[placeholder=Last Name]").setValue("Bulaev");
-        $("#userEmail").setValue("Fooolll@test.com");
-    //    $("[name=gender] [value=other]").click();
-        $("#userNumber").setValue("89649990000");
-        $("#currentAddress").setValue("Penza");
-        $("#subjectsWrapper").$("#subjectsContainer").setValue("Russia");
+       $("#firstName").setValue("Artem");
+       $("#lastName").setValue("Bulaev");
+       $("#userEmail").setValue("Fooolll@test.com");
+       $("[for=gender-radio-1]").click();
+       $("#userNumber").setValue("89649990000");
+       $("#subjectsInput").setValue("Maths").sendKeys(Keys.ENTER);
+       $("#subjectsInput").setValue("bio").sendKeys(Keys.ENTER);
+       $("#subjectsInput").setValue("ch").sendKeys(Keys.ENTER);
+       $("[for=hobbies-checkbox-1]").click();
+       $("[for=hobbies-checkbox-2]").click();
+       $("[for=hobbies-checkbox-3]").click();
+   //    $("#uploadPicture").click();
+       $(".form-control-file").click();
+     //  setValue("d:\Lanit\Test.jpg").sendKeys(Keys.ENTER);
 
+       $("#currentAddress").setValue("Penza");
+
+       // d:\Lanit\Test.jpg
 
     }
 }
