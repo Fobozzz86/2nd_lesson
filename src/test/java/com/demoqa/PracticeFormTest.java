0d17f39c3187a292eb6aa69c84b7695961f8ae26
Artem Bu <fobozzz@yandex.ru>
2023-07-03 13:18:51 +0300

    add fixed banner. (ошибка element click intercepted)

--- src/test/java/com/demoqa/PracticeFormTest.java
+++ src/test/java/com/demoqa/PracticeFormTest.java
@@ -11,6 +11,9 @@ public class PracticeFormTest extends TestBase {
     @Test
     void PractFormTest() {
        open("https://demoqa.com/automation-practice-form");
+ //    Тест может упасть из-за всплывающих баннеров (ошибка element click intercepted), можно добавить
+       executeJavaScript("$('#fixedban').remove()");
+       executeJavaScript("$('footer').remove()");
        $("#firstName").setValue("Artem");
        $("#lastName").setValue("Bulaev");
        $("#userEmail").setValue("Fooolll@test.com");
@@ -21,8 +24,8 @@ public class PracticeFormTest extends TestBase {
        $("#dateOfBirthInput").sendKeys(Keys.ENTER);
 
  //      $("[id=dateOfBirthInput]").click();
- //      $(".react-datepicker__month-select").setValue("3");
- //      $(".react-datepicker__year-select").setValue("1990");
+ //      $(".react-datepicker__month-select").setValue("3"); selectOption("April")
+ //      $(".react-datepicker__year-select").setValue("1990"); selectOption("1990")
  //      $(".react-datepicker__day--010").click();
 
        $("#subjectsInput").setValue("Maths").sendKeys(Keys.ENTER);
@@ -37,6 +40,13 @@ public class PracticeFormTest extends TestBase {
        $("#react-select-3-input").setValue("NCR").sendKeys(Keys.ENTER);
        $("#react-select-4-input").setValue("Delhi").sendKeys(Keys.ENTER);
        $("#submit").click();
+
+       // Выбираем состояние и город
+ //      $("[id=state]").scrollTo().click();
+ //      $("[id=react-select-3-option-2]").click();
+ //      $("[id=city]").scrollTo().click();
+ //      $("[id=react-select-4-option-1]").click();
+
        $(".table-hover").shouldHave(text("Artem Bulaev"), text("Fooolll@test.com"),
                text("Male"), text("8964999000"));
        $("#closeLargeModal").scrollIntoView(true).click(); // прокрутка для видимости кнопки
